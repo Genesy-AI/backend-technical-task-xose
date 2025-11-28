c14a6fecdd7f627f6dc71a06e7b1b89fc0e55f8f
xoselolo <1a09lopxose@gmail.com>
2025-11-28 13:39:08 +0100

    test: pipeline tested to fail with incorrect db migrations or schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -34,7 +34,6 @@ app.post('/leads', async (req: Request, res: Response) => {
       firstName: String(name),
       lastName: String(lastName),
       email: String(email),
-      testField: '',
     },
   })
   res.json(lead)
@@ -231,7 +230,6 @@ app.post('/leads/bulk', async (req: Request, res: Response) => {
             jobTitle: lead.jobTitle ? lead.jobTitle.trim() : null,
             countryCode: lead.countryCode ? lead.countryCode.trim() : null,
             companyName: lead.companyName ? lead.companyName.trim() : null,
-            testField: '',
           },
         })
         importedCount++
